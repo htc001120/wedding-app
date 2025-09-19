dd6d92b7b927bf572601282ce721e92e367affab
Ming <>
2025-09-19 11:35:28 +0100

    no zoom

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,8 +29,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
+      <head>
+        <meta
+          name='viewport'
+          content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0'
+        />
+      </head>
       <body
         className={`${poppins.variable} ${msMadi.className} ${msMadi.variable}`}
+        style={{ touchAction: 'pan-x pan-y' }}
       >
         <LangProvider>
           <LocalizationProvider>
